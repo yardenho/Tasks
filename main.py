a7d735a38db8eb420c714ef924dbc5205be22e53
yardenho <hovav4321@gmail.com>
2023-01-30 19:27:37 +0200

    ex2 - summer

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from priceCheck import priceCheckTest
+from summer import summer, tailCellRecursiveSummer
 
+# ex1
 priceCheckTest()
+
+# ex2
+print(summer(2347623))
+print(tailCellRecursiveSummer(2347623, 0))
+
 
---
+++ summer.py
@@ -0,0 +1,12 @@
+# regular recursive
+def summer(num):
+    if num == 0:
+        return 0
+    return summer(num//10) + num % 10
+
+
+# if we want tail cell recursive
+def tailCellRecursiveSummer(num, b):
+    if num == 0:
+        return b
+    return tailCellRecursiveSummer(num//10, b + num % 10)
